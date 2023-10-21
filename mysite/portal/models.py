403279d003b988b45532988469653eeead61559e
bartekuznik <bartek.krecio@onet.pl>
2023-10-21 17:27:49 +0200

    first database

--- mysite/portal/models.py
+++ mysite/portal/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.fields.files import ImageField
 
 # Create your models here.
+
+RATE_CHOICE = (
+    ("1", "1"), 
+    ("2", "2"), 
+    ("3", "3"), 
+    ("4", "4"), 
+    ("5", "5"),
+)
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=200)
+    main_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.TextField(blank = True)
+    image = models.ImageField(upload_to='profiles/%Y/%m/%d', blank = True)
+    
+class Product(models.Model):
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='products/%Y/%m/%d')
+    description = models.TextField(blank = True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created = models.DateField(auto_now_add=True)
+    quantity = models.IntegerField(default=1)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+    
+
+class ProductRevirev(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(blank=True)
+    reviev_choice = models.IntegerField(choices=RATE_CHOICE, default='5')
+
+    def __str__(self):
+        return self.reviev_choice
