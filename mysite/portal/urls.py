a1cecafbfdc875818266cfbd547fbdf379f16211
bartekuznik <bartek.krecio@onet.pl>
2023-10-24 22:42:07 +0200

    create, update, delete product

--- mysite/portal/urls.py
+++ mysite/portal/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from portal.views import ProductCreateView, ProductUpdateView, ProductDeleteView
 
 app_name = 'portal'
 
@@ -9,4 +10,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='portal/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='portal/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
+    path('product/new/', ProductCreateView.as_view(), name='product-create'),
+    path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product-update'),
+    path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product-delete'),
 ]
--- mysite/portal/views.py
+++ mysite/portal/views.py
@@ -1,6 +1,10 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import *
 from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 
@@ -19,3 +23,37 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'portal/register.html', {'form': form})
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    model = Product
+    fields = ['sub_category', 'name', 'image', 'description', 'price', 'quantity', 'available']
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class ProductUpdateView(LoginRequiredMixin, UserPassesTestMixin,CreateView):
+    model = Product
+    template_name ='portal/product_update.html'
+    fields = ['sub_category', 'name', 'image', 'description', 'price', 'quantity', 'available']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        product = self.get_object()
+        if self.request.user == product.author:
+            return True
+        else:
+            return False
+        
+class ProductDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Product
+    success_url ='/'
+
+    def test_func(self):
+        product = self.get_object()
+        if self.request.user == product.author:
+            return True
+        else:
+            return False
